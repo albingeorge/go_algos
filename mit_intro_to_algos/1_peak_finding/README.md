0141e744dff65bf539866c402298cfb373852bb1
Albin George <albinin0002@gmail.com>
2020-08-09 17:58:59 +0530

    Peak finding

---
+++ mit_intro_to_algos/1_peak_finding/README.md
@@ -0,0 +1,7 @@
+### Problem statement
+
+Given a set of numbers, find a peak.
+
+Definition of a peak position:
+
+Position i is a peak, if input[i] >= input[i-1] and input[i] >= input[i+1]
---
+++ mit_intro_to_algos/1_peak_finding/peak_finding.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	input := []int{1, 3, 5, 5, 4, 6, 3, 2, 1, 4, 7, 3, 4, 2, 5}
+
+	last := len(input) - 1
+	pos := peakFindRecursive(input, 0, last)
+
+	fmt.Println(pos)
+}
+
+func peakFindRecursive(input []int, start int, end int) int {
+	mid := start + ((end - start) / 2)
+
+	if (end - start) == 0 {
+		return start
+	} else if end-start == 1 {
+		if input[start] < input[end] {
+			return end
+		}
+		return start
+	}
+
+	if input[mid-1] <= input[mid] && input[mid] >= input[mid+1] {
+		return mid
+	} else if input[mid-1] < input[mid] {
+		return peakFindRecursive(input, mid, end)
+	} else {
+		return peakFindRecursive(input, start, mid-1)
+	}
+}
