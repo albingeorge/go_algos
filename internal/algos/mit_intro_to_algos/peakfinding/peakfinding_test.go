86e3f74f4dec0251d4b208ce115ee03eebc0a250
Albin George <albinin0002@gmail.com>
2021-01-29 02:46:35 +0530

    Add tests for brute force

--- internal/algos/mit_intro_to_algos/peakfinding/peakfinding_test.go
+++ internal/algos/mit_intro_to_algos/peakfinding/peakfinding_test.go
@@ -14,11 +14,29 @@ func TestPeakFinding(t *testing.T) {
 	}
 }
 
+func TestPeakFindingBrute(t *testing.T) {
+	input := []int{1, 3, 5, 6, 4, 6, 3, 2, 1, 4, 7, 3, 4, 2, 5}
+	pos, _ := PeakFindBruteForce(input)
+
+	if pos != 3 {
+		t.Errorf("Peak finding failed, expected: %v; got: %v", 3, pos)
+	}
+}
+
 func BenchmarkPeakFinding(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for i := 0; i < 100; i++ {
 		b.StopTimer()
 		input := rand.Perm(10)
 		b.StartTimer()
 		Peakfind(input)
+	}
+}
+
+func BenchmarkPeakFindingBrute(b *testing.B) {
+	for i := 0; i < 100; i++ {
+		b.StopTimer()
+		input := rand.Perm(10)
+		b.StartTimer()
+		PeakFindBruteForce(input)
 	}
 }
