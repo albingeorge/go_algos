90fc744aa7536d02e95140aa3cf06b6d431d96a2
Albin George <albinin0002@gmail.com>
2023-07-10 04:43:48 +0200

    Add more nodes in test case

--- internal/datastructures/binarytree/binarytree_test.go
+++ internal/datastructures/binarytree/binarytree_test.go
@@ -5,17 +5,29 @@ import "testing"
 func TestBinaryTreePreorder(t *testing.T) {
 	tree := BinaryTree{}
 	tree.Root = &TreeNode{
-		Data: 4,
+		Data: 1,
 		Left: &TreeNode{
-			Data: 3,
+			Data: 2,
+			Left: &TreeNode{
+				Data: 6,
+			},
+			Right: &TreeNode{
+				Data: 4,
+			},
 		},
 		Right: &TreeNode{
-			Data: 5,
+			Data: 3,
+			Left: &TreeNode{
+				Data: 5,
+			},
+			Right: &TreeNode{
+				Data: 7,
+			},
 		},
 	}
 
 	got := tree.PreorderTraversal()
-	res := []int{4, 3, 5}
+	res := []int{1, 2, 6, 4, 3, 5, 7}
 	for i, v := range got {
 		if v != res[i] {
 			t.Errorf("expected: %v, received: %v", res, got)
